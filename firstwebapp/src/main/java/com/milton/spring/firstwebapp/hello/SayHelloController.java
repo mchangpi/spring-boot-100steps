c09a23cc5e59262a9a2bb9a199e65a247a7c509c
Milton Chang <mchangpi@gmail.com>
2024-04-05 00:48:50 +0800

    L31 Enhancing Spring MVC Controller to provide HTML response

--- firstwebapp/src/main/java/com/milton/spring/firstwebapp/hello/SayHelloController.java
+++ firstwebapp/src/main/java/com/milton/spring/firstwebapp/hello/SayHelloController.java
@@ -1,23 +1,26 @@
 package com.milton.spring.firstwebapp.hello;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 public class SayHelloController {
 
-//"say-hello" => "Hello! What are you learning today?"
+  //"say-hello" => "Hello! What are you learning today?"
 
-//say-hello
-// http://localhost:8080/say-hello
+  //say-hello
+  // http://localhost:8080/say-hello
   @RequestMapping("say-hello")
-//  @ResponseBody
+  @ResponseBody
   public String sayHello() {
     return "Hello! What are you learning today?";
   }
 
   @RequestMapping("say-hello-html")
-//  @ResponseBody
+  @ResponseBody
   public String sayHelloHtml() {
     StringBuffer sb = new StringBuffer();
     sb.append("<html>");
@@ -32,14 +35,4 @@ public class SayHelloController {
     return sb.toString();
   }
 
-//
-// "say-hello-jsp" => sayHello.jsp 
-// /src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
-// /src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
-// /src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
-// /src/main/resources/META-INF/resources/WEB-INF/jsp/todos.jsp
-  @RequestMapping("say-hello-jsp")
-  public String sayHelloJsp() {
-    return "sayHello";
-  }
 }
