796e5b300686a62e5b187bfb917cd386f01ed559
Milton Chang <mchangpi@gmail.com>
2024-04-05 16:50:09 +0800

    L43 Displaying Login Credentials in a JSP using ModelMap

--- firstwebapp/src/main/java/com/milton/spring/firstwebapp/login/LoginController.java
+++ firstwebapp/src/main/java/com/milton/spring/firstwebapp/login/LoginController.java
@@ -3,31 +3,39 @@ package com.milton.spring.firstwebapp.login;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
   private Logger logger = LoggerFactory.getLogger(getClass());
 
-  @RequestMapping("login")
+  @RequestMapping(value = "login", method = RequestMethod.GET)
   public String loginPage() {
     return "login";
   }
+
+  @RequestMapping(value = "login", method = RequestMethod.POST)
+  public String welcomePage(@RequestParam String name, 
+      @RequestParam String password, ModelMap model) {
+
+    model.put("name", name);
+    model.put("password", password);
+    
+    return "welcome";
+  }
+
 }
 
 /*
-//import org.springframework.ui.ModelMap;
-//import org.springframework.web.bind.annotation.RequestParam;
-@Controller
-public class LoginController {
-  private Logger logger = LoggerFactory.getLogger(getClass());
-
-  @RequestMapping("login")
-  public String loginPage(@RequestParam String name, ModelMap model) {
-    System.out.println("println: req.query?name=" + name);
-    logger.info("logger: req.query?name=" + name);
-
-    model.put("modelName", name);
-    return "login";
-  }
-}*/
+ * @Controller public class LoginController { private Logger logger =
+ * LoggerFactory.getLogger(getClass());
+ * 
+ * @RequestMapping("login") public String loginPage(@RequestParam String name,
+ * ModelMap model) { System.out.println("println: req.query?name=" + name);
+ * logger.info("logger: req.query?name=" + name);
+ * 
+ * model.put("modelName", name); return "login"; } }
+ */
---
+++ firstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> My Welcome Page - JSP</title>
+	</head>
+	<body>
+		Welcome to My Learn Page, ${name} !
+		<div>password (should not display): ${password}</div> 
+	</body>
+</html>
