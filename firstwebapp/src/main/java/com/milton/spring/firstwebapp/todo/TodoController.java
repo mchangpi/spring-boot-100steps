43f14b79fae70a5cf86ba61230e1bba7c1a8937a
Milton Chang <mchangpi@gmail.com>
2024-04-06 08:50:43 +0800

    L53 Add a New Todo with GET/POST

--- firstwebapp/src/main/java/com/milton/spring/firstwebapp/todo/TodoController.java
+++ firstwebapp/src/main/java/com/milton/spring/firstwebapp/todo/TodoController.java
@@ -1,17 +1,19 @@
 package com.milton.spring.firstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.ui.ModelMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @SessionAttributes("name")
 public class TodoController {
-  
+
   private TodoService todoService;
-  
+
   // constructor injection
   public TodoController(TodoService todoService) {
     super();
@@ -22,8 +24,22 @@ public class TodoController {
   public String listAllTodos(ModelMap model) {
     List<Todo> todos = todoService.findByUsername("milton");
     model.addAttribute("todos", todos);
-    
+
     return "listTodos";
   }
-  
+
+  // GET
+  @RequestMapping(value = "add-todo", method = RequestMethod.GET)
+  public String getAddTodo(ModelMap model) {
+    String username = (String) model.get("name"); // from session
+//    Todo todo = new Todo(0, username, "Default Desc", LocalDate.now().plusYears(1), false);
+//    model.put("todo", todo);
+    return "addTodo";
+  }
+
+  // POST
+  @RequestMapping(value = "add-todo", method = RequestMethod.POST)
+  public String postAddTodo(ModelMap model) {
+    return "redirect:list-todos";
+  }
 }
---
+++ firstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/addTodo.jsp
@@ -0,0 +1,40 @@
+<%@ taglib prefix="c" uri="jakarta.tags.core" %> 
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <link
+      href="webjars/bootstrap/5.1.3/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="webjars/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.standalone.min.css"
+      rel="stylesheet"
+    />
+    <title>Add Todo Page</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Enter Todo Details:</h1>
+
+      <form method="post" >
+        Description:
+        <input type="text" path="description" required="required" />
+        <input type="hidden" path="id" />
+        <input type="hidden" path="done" />
+        <input type="submit" class="btn btn-success" />
+      </form>
+      <!--
+      <form:form method="post" modelAttribute="addTodo">
+        Description:
+        <form:input type="text" path="description" required="required" />
+        <form:input type="hidden" path="id" />
+        <form:input type="hidden" path="done" />
+        <input type="submit" class="btn btn-success" />
+      </form:form>-->
+      <script src="webjars/bootstrap/5.1.3/js/bootstrap.min.js"></script>
+      <script src="webjars/jquery/3.6.0/jquery.min.js"></script>
+      <script src="webjars/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+    </div>
+  </body>
+</html>
