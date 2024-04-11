1fc99236487c3eb705928de5a762b7fb4d72beaf
Milton Chang <mchangpi@gmail.com>
2024-04-12 06:20:04 +0800

    L133 Add some UserDetails with CommandLineRunner

---
+++ rest-api/src/main/java/com/milton/springboot/restapi/user/UserDetailsCommandLineRunner.java
@@ -0,0 +1,36 @@
+package com.milton.springboot.restapi.user;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsCommandLineRunner implements CommandLineRunner {
+
+  private Logger logger = LoggerFactory.getLogger(getClass());
+
+  private UserDetailsRepository repository;
+
+  public UserDetailsCommandLineRunner(UserDetailsRepository repository) {
+    super();
+    this.repository = repository;
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    repository.save(new UserDetails("Ranga", "Admin"));
+    repository.save(new UserDetails("Ravi", "Admin"));
+    repository.save(new UserDetails("John", "User"));
+    
+    //List<UserDetails> users = repository.findAll();
+    
+    List<UserDetails> users = repository.findByRole("Admin");
+    
+    users.forEach(user -> logger.info("admin: " + user.toString()));
+    
+  }
+
+}
--- rest-api/src/main/java/com/milton/springboot/restapi/user/UserDetailsRepository.java
+++ rest-api/src/main/java/com/milton/springboot/restapi/user/UserDetailsRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// NOT necessary to put the @Repository annotation on interfaces that extend JpaRepositor
+
 public interface UserDetailsRepository
     extends JpaRepository<UserDetails, Long> {
   List<UserDetails> findByRole(String role);
