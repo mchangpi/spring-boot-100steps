84184fb6c0fff861794b7197d7b7ba4ca6dde685
Milton Chang <mchangpi@gmail.com>
2024-04-12 08:49:11 +0800

    L138 Improve Junit Asserts by Asserting Response Headers and Status Code

--- rest-api/src/test/java/com/milton/springboot/restapi/survey/SurveyResourceIT.java
+++ rest-api/src/test/java/com/milton/springboot/restapi/survey/SurveyResourceIT.java
@@ -3,6 +3,7 @@ package com.milton.springboot.restapi.survey;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
@@ -53,6 +54,10 @@ public class SurveyResourceIT {
         }
         """;
 
+    assertEquals("application/json", 
+        responseEntity.getHeaders().get("Content-Type").get(0));
+    assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+    
     JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
     
     /*
@@ -62,5 +67,76 @@ public class SurveyResourceIT {
     System.out.println("body: " + responseEntity.getBody());
     */
   }
+
+/* 
+  @Test
+  void retrieveAllSurveyQuestions_basicScenario() throws JSONException {
+    
+    ResponseEntity<String> responseEntity = template.getForEntity(GENERIC_QUESTIONS_URL, String.class);
+
+    String expectedResponse =
+        """
+            [
+              {
+                "id": "Question1"
+              },
+              {
+                "id": "Question2"
+              },
+              {
+                "id": "Question3"
+              }
+            ]
+        
+        """;
+
+    assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+    assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+    
+    JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
+     
+  }
+
+  
+  @Test
+  void addNewSurveyQuestion_basicScenario() {
+
+    String requestBody = """
+          {
+            "description": "Your Favorite Language",
+            "options": [
+              "Java",
+              "Python",
+              "JavaScript",
+              "Haskell"
+            ],
+            "correctAnswer": "Java"
+          }
+        """;
+
+    
+    //
+    
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Content-Type", "application/json");
+    
+    HttpEntity<String> httpEntity = new HttpEntity<String>(requestBody, headers);
+    
+    ResponseEntity<String> responseEntity 
+      = template.exchange(GENERIC_QUESTIONS_URL, HttpMethod.POST, httpEntity, String.class);
+    
+    assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+    
+    String locationHeader = responseEntity.getHeaders().get("Location").get(0);
+    assertTrue(locationHeader.contains("/surveys/Survey1/questions/"));
+    
+    //DELETE
+    //locationHeader
+    
+    template.delete(locationHeader);
+    
+  }
+*/  
+}
 
 }
