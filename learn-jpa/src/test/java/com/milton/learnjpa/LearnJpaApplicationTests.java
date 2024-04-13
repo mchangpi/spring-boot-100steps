0569800fc40a5f85011906fc0a662d5104d1eed9
Milton Chang <mchangpi@gmail.com>
2024-04-13 08:21:02 +0800

    L170 Configure H2 Console, Create Course Table with schema.sql

---
+++ learn-jpa/src/test/java/com/milton/learnjpa/LearnJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milton.learnjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
