c388a0299396af219218fb1a3d39d8fcf4b3e028
Milton Chang <mchangpi@gmail.com>
2024-04-13 09:47:45 +0800

    L173 Insert and Delete Course with JdbcTemplate

--- learn-jpa/src/main/java/com/milton/learnjpa/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa/src/main/java/com/milton/learnjpa/course/jdbc/CourseJdbcRepository.java
@@ -4,19 +4,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.milton.learnjpa.course.Course;
+
 @Repository
 public class CourseJdbcRepository {
-  
+
   @Autowired
   private JdbcTemplate springJdbcTemplate;
-  
-  private static String INSERT_QUERY = 
-      """
+
+  private static String INSERT_QUERY = """
         insert into course (id, name, author)
-        values (1, 'Learn AWS', 'milton');
+        values (?, ?, ?);
       """;
-  
-  public void insert() {
-    springJdbcTemplate.update(INSERT_QUERY);
+
+  private static String DELETE_QUERY = """
+        delete from course
+        where id = ?
+      """;
+
+  public void insert(Course course) {
+    springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(),
+        course.getAuthor());
+  }
+
+  public void deleteById(long id) {
+    springJdbcTemplate.update(DELETE_QUERY, id);
   }
 }
