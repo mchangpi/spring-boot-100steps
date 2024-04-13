3314dd580a2bac576beb9b2a8b6febb2b45d278c
Milton Chang <mchangpi@gmail.com>
2024-04-13 10:16:54 +0800

    L174 Query Course with JdbcTemplate

--- learn-jpa/src/main/java/com/milton/learnjpa/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa/src/main/java/com/milton/learnjpa/course/jdbc/CourseJdbcRepository.java
@@ -1,6 +1,7 @@
 package com.milton.learnjpa.course.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -22,6 +23,11 @@ public class CourseJdbcRepository {
         where id = ?
       """;
 
+  private static String SELECT_QUERY = """
+        select * from course
+        where id = ?
+      """;
+
   public void insert(Course course) {
     springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(),
         course.getAuthor());
@@ -29,5 +35,10 @@ public class CourseJdbcRepository {
 
   public void deleteById(long id) {
     springJdbcTemplate.update(DELETE_QUERY, id);
+  }
+
+  public Course findById(long id) {
+    return springJdbcTemplate.queryForObject(SELECT_QUERY,
+        new BeanPropertyRowMapper<>(Course.class), id);
   }
 }
